fe78b08e81482a05f91f5eadcdc7412c924c02b6
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2022-06-05 12:09:13 -0700

    Create dynamic loading with Jinja
    All pages will now take data from a json file, "profile.json", and dynamically load information like name, email, projects, experience, and hobbies. This makes the project easy to customize for each person!

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,26 @@
 import os
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
+profile = json.loads(open("app/profile.json", "r").read())
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return redirect(url_for('home'))
 
 
 @app.route('/home')
 def home():
-    return render_template('home.html', title="hey", url=os.getenv("URL"))
+    return render_template('home.html', title="hey", profile=profile, url=os.getenv("URL"))
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About Lorem", url=os.getenv("URL"))
+    return render_template("about.html", title="About {{ profile['name'] }}", profile=profile, url=os.getenv("URL"))
 
 @app.route("/hobbies")
 def hobbies():
-    return render_template("hobbies.html", title="Lorem's Hobbies", url=os.getenv("URL"))
+    return render_template("hobbies.html", title="{{ profile['name'] }}'s Hobbies", profile=profile, url=os.getenv("URL"))
