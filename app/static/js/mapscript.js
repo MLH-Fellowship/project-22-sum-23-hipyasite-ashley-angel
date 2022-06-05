af8ac4a2260393571d33a959041fd6e2aa835253
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2022-06-04 19:36:05 -0700

    Add map click and scroll features

--- app/static/js/mapscript.js
+++ app/static/js/mapscript.js
@@ -5,5 +5,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-var marker1 = L.marker([38.54220, -121.74960]).addTo(map);
-marker1.bindPopup("<div class='mappopup'><b>University of California, Davis</b><br>I go to school here!</div>")
+var marker1 = L.marker([38.54220, -121.74960]).addTo(map).on("click", () => {
+    document.getElementById('hobby1').scrollIntoView();
+});
+
+var marker2 = L.marker([21.085,-86.847]).addTo(map).on("click", () => {
+    document.getElementById('hobby2').scrollIntoView();
+});
+
+var marker3 = L.marker([40.0139, -105.2748]).addTo(map).on("click", () => {
+    document.getElementById('hobby3').scrollIntoView();
+});
+
+var marker4 = L.marker([55.9521, -3.1860]).addTo(map).on("click", () => {
+    document.getElementById('hobby4').scrollIntoView();
+});
