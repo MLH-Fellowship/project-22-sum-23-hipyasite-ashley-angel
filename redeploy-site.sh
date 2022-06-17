de231e113ba0ea058ca81ce733c34e63ef3e584d
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2022-06-16 23:51:41 -0700

    Create redeploy-site.sh
    This script will kill all existing tmux sessions, refresh the repository, and reinstall requirements, and launch a new tmux session with the correct environment and run the flask server.

---
+++ redeploy-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmux kill-server
+cd ~/project-22-sum-23-hipyasite-ashley-angel
+git fetch && git reset origin/main --hard
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+tmux new "python -m venv python3-virtualenv;source python3-virtualenv/bin/activate;export FLASK_ENV=development;flask run"
