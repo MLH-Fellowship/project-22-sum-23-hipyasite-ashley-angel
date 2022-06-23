5eb417dede0922ab049f54330fb04be1536d883b
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2022-06-22 23:15:03 -0700

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
 cd ~/project-22-sum-23-hipyasite-ashley-angel
 git fetch && git reset origin/main --hard
 python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-tmux new "python3 -m venv python3-virtualenv;source python3-virtualenv/bin/activate;export FLASK_ENV=development;flask run --host=0.0.0.0"
+systemctl restart myportfolio
