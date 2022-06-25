ba62a697ac893baa0e921c7cad379c2c307d8622
angel zambrano <angeldzambrano99@gmail.com>
2022-06-25 12:27:28 -0700

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,7 @@
-tmux new -s my_session
 cd project-22-sum-23-hipyasite-ashley-angel
 git fetch && git reset origin/main --hard
 python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
-flask run --host=0.0.0.0
-tmux detach
+systemctl restart myportfolio
