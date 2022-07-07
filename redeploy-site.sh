7694ec965ff39b97ee731687f4e1f54a9a1fa076
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2022-07-06 22:07:24 -0700

    Make redeploy script *slightly* more convenient

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,3 +4,4 @@ cd ~/project-22-sum-23-hipyasite-ashley-angel
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
+chmod +x ./redeploy-site.sh
